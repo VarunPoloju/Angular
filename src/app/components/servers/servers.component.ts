44cc3159c47a1affcd7023ed0916625adc4ea73e
Varun <Varunpoloju966@gmail.com>
2021-11-29 16:58:57 +0530

    adding index for ngFor

--- src/app/components/servers/servers.component.ts
+++ src/app/components/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = ' ';
-  serverName= ' ';
+  serverName= 'Test Server';
   serverCreated: boolean = false;
   servers = ['Test server 1', 'Test server 2'];
 
