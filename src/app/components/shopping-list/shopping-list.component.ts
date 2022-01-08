0937c1dc83d5758f57ea897bbdca9bdb44e78a85
Varun <Varunpoloju966@gmail.com>
2022-01-08 18:52:13 +0530

    allow user to add ingredients in shopping list using Event binding

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -8,12 +8,15 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients :Ingredient[] = [
-    new Ingredient('Apples' , 5),
-    new Ingredient('Oranges' , 10)
+    // new Ingredient('Apples' , 5),
+    // new Ingredient('Oranges' , 10)
   ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onIngredientAdded(ingredient:Ingredient){
+    this.ingredients.push(ingredient)
+  }
 }
