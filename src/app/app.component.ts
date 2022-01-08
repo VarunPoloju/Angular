2053c38d1400d16b64606c536dfd25401bd3ac3f
Varun <Varunpoloju966@gmail.com>
2022-01-08 12:51:40 +0530

    View encapsulation-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent {
 
   onBlueprintCreated(Blueprintdata:{serverName :string,serverContent:string}){
     this.serverElements.push({
-      type: 'server',
+      type: 'blueprint',
       name: Blueprintdata.serverName,
       content: Blueprintdata.serverContent
     });
