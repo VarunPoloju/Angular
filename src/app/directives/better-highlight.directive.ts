d816b3cb0780343d3071958fed367d174af49c54
Varun <Varunpoloju966@gmail.com>
2022-01-09 13:29:12 +0530

    Using host Binding to set style  properties

--- src/app/directives/better-highlight.directive.ts
+++ src/app/directives/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -6,16 +6,19 @@ import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular
 export class BetterHighlightDirective implements OnInit{
 
   constructor(private renderer : Renderer2,private elementref : ElementRef) { }
-
+  @HostBinding('style.backgroundColor') backgroundColor : string = 'transparent';
   ngOnInit() {
       // this.renderer.setStyle(this.elementref.nativeElement , 'backgroundColor', 'red')
   }
 
   @HostListener('mouseover') mouseover(){
-    this.renderer.setStyle(this.elementref.nativeElement , 'backgroundColor', 'red')
+    // this.renderer.setStyle(this.elementref.nativeElement , 'backgroundColor', 'red')
+    this.backgroundColor = 'yellow'
   }
 
   @HostListener('mouseleave') mouseleave(){
-    this.renderer.setStyle(this.elementref.nativeElement , 'backgroundColor', 'yellow')
+    // this.renderer.setStyle(this.elementref.nativeElement , 'backgroundColor', 'yellow')
+    this.backgroundColor = 'grey'
+
   }
 }
